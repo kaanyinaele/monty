f8313bc2298cd96155ce7d4c30bde8006ba5ece0
kaanyinaele <kaanyinaele@gmail.com>
2024-01-18 00:43:56 -0800

    more fixes

--- main.c
+++ main.c
@@ -6,40 +6,62 @@ bus_t bus = {NULL, NULL, NULL, 0};
 * @argv: monty file location
 * Return: 0 on success
 */
-int main(int argc, char *argv[])
-{
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+// main.c
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-return (0);
+
+int main(int argc, char *argv[]) {
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int line_number = 0;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    stack.top = -1; // Initialize stack top
+
+    while ((read = getline(&line, &len, file)) != -1) {
+        line_number++;
+
+        // Remove trailing newline character
+        if (line[read - 1] == '\n') {
+            line[read - 1] = '\0';
+        }
+
+        // Parse the command and argument
+        char command[10];
+        char arg[50];
+
+        if (sscanf(line, "%s %s", command, arg) == 2) {
+            if (strcmp(command, "push") == 0) {
+                push(arg, line_number);
+            } else if (strcmp(command, "pall") == 0) {
+                pall();
+            } else {
+                fprintf(stderr, "L%d: Unknown command %s\n", line_number, command);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            fprintf(stderr, "L%d: Invalid command format\n", line_number);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Close the file
+    fclose(file);
+
+    // Free allocated memory
+    free(line);
+
+    return 0;
 }
+
--- push.c
+++ push.c
@@ -5,10 +5,27 @@
  * @counter: line_number
  * Return: no return
 */
-void push(int value, int line_number) {
+// push.c
+
+
+void push(char *arg, int line_number) {
+    if (!arg || !isdigit(*arg) && *arg != '-' && *arg != '+') {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    int value = atoi(arg);
+    
+    if (value == 0 && *arg != '0') {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
     if (stack.top == STACK_SIZE - 1) {
         fprintf(stderr, "L%d: Error: Stack overflow\n", line_number);
         exit(EXIT_FAILURE);
     }
+
     stack.stack[++stack.top] = value;
 }
+
