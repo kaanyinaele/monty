44811f287d4b769c00e352117fad610def29bb37
kaanyinaele <kaanyinaele@gmail.com>
2024-01-18 00:13:47 -0800

    push and pall files

--- pall.c
+++ pall.c
@@ -5,17 +5,9 @@
  * @counter: no used
  * Return: no return
 */
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
-
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+void pall() {
+    int i;
+    for (i = stack.top; i >= 0; i--) {
+        printf("%d\n", stack.stack[i]);
+    }
 }
--- push.c
+++ push.c
@@ -5,33 +5,10 @@
  * @counter: line_number
  * Return: no return
 */
-void f_push(stack_t **head, unsigned int counter)
-{
-	int n, j = 0, flag = 0;
-
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
+void push(int value, int line_number) {
+    if (stack.top == STACK_SIZE - 1) {
+        fprintf(stderr, "L%d: Error: Stack overflow\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    stack.stack[++stack.top] = value;
 }
